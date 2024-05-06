4963f673cd19fd21d31376da0811acd8bebf83a9
Alex Delaveau <alex26delaveau@gmail.com>
2024-05-07 00:49:15 +0200

    Quality picker working but needs to work on merging audio and video for quality over 360p

--- app.js
+++ app.js
@@ -1,50 +1,86 @@
-//Create basic webserver with 2 endpoints /download/${format}/?url=${encodeURIComponent(url)}&quality=${quality} with format being mp3 or mp4
-//get port from PORT .env
 const PORT = process.env.PORT || 3000;
 const express = require("express");
 const ytdl = require("ytdl-core");
+const contentDisposition = require("content-disposition");
 
 const app = express();
 app.use(express.static("public"));
 
-app.get("/download/:format", async (req, res) => {
-  const { url, quality } = req.query;
-  const format = req.params.format;
-  console.log(url, quality, format);
-  // Intégrer votre logique pour traiter la demande de téléchargement
-  // const videoInfo = await (await ytdl.getBasicInfo(decodeURIComponent(url))).formats
-  if (format === "mp3") {
-    //res send the result of the promise from downloadAudio
-    const stream = await downloadAudio(url);
-    res.set("Content-Type", "audio/mpeg");
-    res.set("Content-Disposition", "attachment; filename=audio.mp3");
-    console.log(stream)
-    res.send(stream)
-  }
-  // Simuler une réponse
-  
-});
-
 const optsByFormat = new Map([
-  // maps container to ytdl-core `ytdl` options
   ["mp3", { format: "mp3", quality: "highestaudio", filter: "audioonly" }],
-  ["mp4", { format: "mp4", quality: "highest" }],
 ]);
 
-const downloadAudio = async (url) => {
-  //Get content length
+const downloadAudio = async (res, req) => {
+  const url = decodeURIComponent(req.query.url);
 
-  const info = await ytdl.getInfo(url);
-  const format = ytdl.chooseFormat(info.formats, { quality: "highestaudio" });
-  const itag = format.itag;
+  if (!ytdl.validateURL(url)) return res.status(400).send("Invalid link!");
 
-  //Download audio using itag 
+  const audioOpts = optsByFormat.get("mp3");
+  const audioInfo = await ytdl.getInfo(url);
+  const audioFormat = ytdl.chooseFormat(audioInfo.formats, audioOpts);
+  const title = audioInfo.videoDetails.title;
+  const filename = `${title}.mp3`;
+  console.log(title)
 
-   return ytdl(url, { filter: (format) => format.itag === itag });
+  if (!audioFormat) {
+    throw new Error("No audio format found");
+  }
+
+  res.set("Content-Length", audioFormat.contentLength);
+  res.set("Content-Type", "audio/mpeg");
+  res.set("Content-Disposition", contentDisposition(filename, { type: 'attachment' }));
+
+  ytdl(url, audioOpts).pipe(res);
 };
 
-app.get("", (req, res) => {
-  res.send("Hello World");
+const downloadVideo = async (res, req) => {
+  const url = decodeURIComponent(req.query.url);
+  const quality = req.query.quality || "highest";
+
+  if (!ytdl.validateURL(url)) return res.status(400).send("Invalid link!");
+
+  const videoInfo = await ytdl.getInfo(url);
+
+  const videoFormatRaw = videoInfo.formats.find((format) => {
+    if (format.qualityLabel === quality + 'p') {
+      return format;
+    }
+  });
+
+  const videoFormat = ytdl.chooseFormat(videoInfo.formats, { quality: videoFormatRaw.itag});
+
+
+  const title = videoInfo.videoDetails.title;
+  const filename = `${title}.mp4`;
+
+  if (!videoFormat) {
+    return res.status(404).send(`No video format found for quality ${quality}`);
+  }
+
+  res.set("Content-Type", "video/mp4");
+  res.set("Content-Disposition", contentDisposition(filename, { type: "attachment" }));
+  res.set("Content-Length", videoFormat.contentLength);
+
+  if(!videoFormat.hasAudio){
+    //treat audio and video separately using ffmpeg
+  }
+
+
+  ytdl(url,{ format: videoFormat }).pipe(res);
+};
+
+app.get("/download/:format", async (req, res) => {
+  const format = req.params.format;
+
+  if (!["mp3", "mp4"].includes(format)) {
+    return res.status(400).send("Invalid format");
+  }
+
+  if (format === "mp3") {
+    await downloadAudio(res, req);
+  } else {
+    await downloadVideo(res, req);
+  }
 });
 
 app.listen(PORT, () => {
